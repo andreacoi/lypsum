d1a8688d22e8ec18b6797f02956900fb14e944d8
Andrea Coi <andrea.coi@outlook.it>
2021-02-25 14:14:52 +0100

    test_vcs

--- main.py
+++ main.py
@@ -17,5 +17,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello world')
+    print('vcs_test')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
