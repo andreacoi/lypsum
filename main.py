0bcbc25ef6d5637d2f9a58c9544f8ea31a2f5678
Andrea Coi <andrea.coi@outlook.it>
2021-02-25 14:04:32 +0100

    Kickstarting project

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Maiusc+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Importo sys per la gestione degli argomenti tramite terminale
+import sys
+
+# Importo pyperclip per la gestione della clipboard, xclip è una dipendenza necessaria
+import pyperclip
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Hello world')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
