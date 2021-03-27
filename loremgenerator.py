997fcf454992c94dfb99bb316123bcadab27a720
Andrea <andrea.coi@outlook.it>
2021-03-27 16:20:14 +0100

    Added single big class for elab functions.

--- loremgenerator.py
+++ loremgenerator.py
@@ -1,38 +1,43 @@
-def generator(args):
-    # Carico la prima frase iconica standard di lorem ipsum, ad ogni avvio infatti, il mio lorem ipsum dovrà iniziare con questa frase.
-    first_phrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+from pip._vendor.certifi.__main__ import args
 
-    # Carico il dizionario di parole che serviranno per generare i paragrafi successivi di lorem ipsum
-    dictionary = open("resources/dictionary.txt", "r")
 
-    # Uso read() per leggere il documento ed effettuare lo split delle parole sulla base del trailing char \n
-    lines = dictionary.read()
+class Generator:
 
-    # Creo una lista splittando le linee
-    words = lines.split('\n')
+    def pgenerator(args):
+        # Carico la prima frase iconica standard di lorem ipsum, ad ogni avvio infatti, il mio lorem ipsum dovrà iniziare con questa frase.
+        first_phrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
 
-    # Chiudo il file
-    dictionary.close()
+        # Carico il dizionario di parole che serviranno per generare i paragrafi successivi di lorem ipsum
+        dictionary = open("resources/dictionary.txt", "r")
 
-    # Uso una lista vuota per caricarci dentro la frase completa, ripetendo il ciclo N volte effettuando lo shuffling delle parole (???) posso creare dei paragrafi
-    single_phrase = []
+        # Uso read() per leggere il documento ed effettuare lo split delle parole sulla base del trailing char \n
+        lines = dictionary.read()
 
-    # Utilizzo un contatore aggiuntivo per contare le parole del lorem ipsum. Al momento è hardcoded ma sarà uno degli argv
-    j = 0
-    for i, word in enumerate(words):
-    # Aggiungo la parola alla lista creata fuori dal ciclo
-        single_phrase.append(word)
-    # Aumento il contatore delle parole di 1
-        j = j+1
-    # Inizio un controllo per verificare che le parole create non siano più di 8. (9 con index 0)
-        if j > 8:
-            # se j > 8 ferma il ciclo
-            break
-    # trasformo la lista in una concatenazione di stringhe (stile explode - php), utilizzando lo spazio come delimitatore.
-    str1 = " ".join(single_phrase)
-    # verifico che siano presenti gli argv
-    if (args):
-        # concateno la string iniziale con quella ottenuta e aggiungo il punto alla fine.
-        generated = first_phrase + str1 + "."
-    #ritorno la stringa finale al main
-    return generated
+        # Creo una lista splittando le linee
+        words = lines.split('\n')
+
+        # Chiudo il file
+        dictionary.close()
+
+        # Uso una lista vuota per caricarci dentro la frase completa, ripetendo il ciclo N volte effettuando lo shuffling delle parole (???) posso creare dei paragrafi
+        single_phrase = []
+
+        # Utilizzo un contatore aggiuntivo per contare le parole del lorem ipsum. Al momento è hardcoded ma sarà uno degli argv
+        j = 0
+        for i, word in enumerate(words):
+        # Aggiungo la parola alla lista creata fuori dal ciclo
+            single_phrase.append(word)
+        # Aumento il contatore delle parole di 1
+            j = j+1
+        # Inizio un controllo per verificare che le parole create non siano più di 8. (9 con index 0)
+            if j > 9:
+                # se j > 8 ferma il ciclo
+                break
+        # trasformo la lista in una concatenazione di stringhe (stile explode - php), utilizzando lo spazio come delimitatore.
+        str1 = " ".join(single_phrase)
+        # verifico che siano presenti gli argv
+        if (args):
+            # concateno la string iniziale con quella ottenuta e aggiungo il punto alla fine.
+            generated = first_phrase + str1 + "."
+        #ritorno la stringa finale al main
+        return generated
